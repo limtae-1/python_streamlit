0ddcc1d8bddd8d228aebd2f3dbc9cf5c03c9dbbf
applepy <edh427@naver.com>
2026-01-24 19:44:46 +0900

    Create coi-serviceworker.js

---
+++ coi-serviceworker.js
@@ -0,0 +1,69 @@
+/*! coi-serviceworker v0.1.7 - Guido Zuidhof, licensed under MIT */
+let coepCredentialless = false;
+if (typeof window === 'undefined') {
+  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+  self.addEventListener("message", (ev) => {
+    if (!ev.data) {
+        return;
+    } else if (ev.data.type === "deregister") {
+        self.registration.unregister().then(() => {
+            return self.clients.matchAll();
+        }).then(clients => {
+            clients.forEach(client => client.navigate(client.url));
+        });
+    }
+  });
+
+  self.addEventListener("fetch", function (event) {
+    const r = event.request;
+    if (r.cache === "only-if-cached" && r.mode !== "same-origin") {
+      return;
+    }
+
+    const coep = coepCredentialless ? "credentialless" : "require-corp";
+    const headerOverrides = {
+        "Cross-Origin-Embedder-Policy": coep,
+        "Cross-Origin-Opener-Policy": "same-origin",
+    };
+
+    event.respondWith(
+      fetch(r).then((response) => {
+        if (response.status === 0) {
+          return response;
+        }
+
+        const newHeaders = new Headers(response.headers);
+        for (const key in headerOverrides) {
+            newHeaders.set(key, headerOverrides[key]);
+        }
+
+        return new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
+        });
+      })
+    );
+  });
+} else {
+  (async function () {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register(window.document.currentScript.src).then(
+            (registration) => {
+                console.log("COI Service Worker registered");
+                registration.addEventListener("updatefound", () => {
+                    window.location.reload();
+                });
+                if (registration.active && !navigator.serviceWorker.controller) {
+                    window.location.reload();
+                }
+            },
+            (err) => {
+                console.error("COI Service Worker failed to register", err);
+            }
+        );
+    }
+  })();
+}
